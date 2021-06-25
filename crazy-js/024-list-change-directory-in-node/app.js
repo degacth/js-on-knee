2b10c67d6a0faa25da85d87afee836b32510fa6b
AlexaDee <degacth@yandex.ru>
2021-06-25 15:59:36 +0300

    fix spaces insted of tabs

--- crazy-js/024-list-change-directory-in-node/app.js
+++ crazy-js/024-list-change-directory-in-node/app.js
@@ -11,42 +11,42 @@ const readdir = util.promisify(fs.readdir)
 const commandWithParams = str => str.trim().split(/\s+/)
 
 const commands = {
-	cd(params) {
-		const [dirName] = params
-		if (dirName.startsWith('/')) return process.chdir(dirName)
+  cd(params) {
+    const [dirName] = params
+    if (dirName.startsWith('/')) return process.chdir(dirName)
 
-		process.chdir(
-			path.join(process.cwd(), dirName)
-		)
-	},
+    process.chdir(
+      path.join(process.cwd(), dirName)
+    )
+  },
 
-	async ls() {
-		const files = await readdir(process.cwd())
-		files.forEach(filename => console.log(filename))
-	},
+  async ls() {
+    const files = await readdir(process.cwd())
+    files.forEach(filename => console.log(filename))
+  },
 
-	exit() {
-		process.exit(0)
-	}
+  exit() {
+    process.exit(0)
+  }
 }
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-	prompt: prompt(),
+  input: process.stdin,
+  output: process.stdout,
+  prompt: prompt(),
 })
 
 const nextCommand = async () => {
-	rl.prompt()
+  rl.prompt()
 
-	const [commandResult] = await events.once(rl, 'line')
-	const [commandName, ...params] = commandWithParams(commandResult)
-	const command = commands[commandName] || (() => console.log(`Unknown command ${commandName}`))
+  const [commandResult] = await events.once(rl, 'line')
+  const [commandName, ...params] = commandWithParams(commandResult)
+  const command = commands[commandName] || (() => console.log(`Unknown command ${commandName}`))
 
-	try { await command(params) }
-	catch (e) { console.error('Command error ' + e.message) }
-	rl.setPrompt(prompt())
-	nextCommand()
+  try { await command(params) }
+  catch (e) { console.error('Command error ' + e.message) }
+  rl.setPrompt(prompt())
+  nextCommand()
 }
 
 console.log('Press Ctrl + C for exit')
--- crazy-js/025-read-write-files/app.js
+++ crazy-js/025-read-write-files/app.js
@@ -4,90 +4,90 @@ const db = 'tasks.db'
 const encoding = 'utf-8'
 const mode = { read: 'a+', write: 'w' }
 const withFile = async (fileName, mode, callback) => {
-	const handler = await fs.open(fileName, mode)
-	try {
-		return await callback(handler)
-	}
-	finally {
-		handler.close()
-	}
+  const handler = await fs.open(fileName, mode)
+  try {
+    return await callback(handler)
+  }
+  finally {
+    handler.close()
+  }
 }
 
 /**
  * parseArgs(['search', 'hello', '--all']) === {args: ['search', 'hello'], options: {all: true}}
  */
 const parseArgs = args => args.reduce((acc, item) => {
-	if (item.startsWith('--')) {
-		const [key, value] = item.split('=')
-		acc.options[key.replace(/^-+/, '')] = value || true
-		return acc
-	}
+  if (item.startsWith('--')) {
+    const [key, value] = item.split('=')
+    acc.options[key.replace(/^-+/, '')] = value || true
+    return acc
+  }
 
-	acc.args.push(item)
-	return acc
-}, { options: {}, args: []})
+  acc.args.push(item)
+  return acc
+}, { options: {}, args: [] })
 
 const tasksUI = {
-	showTasks(tasks) {
-		tasks.forEach(task => console.log('%s ' + this.showTaskFormat(task), task.id, task.task))
-	},
+  showTasks(tasks) {
+    tasks.forEach(task => console.log('%s ' + this.showTaskFormat(task), task.id, task.task))
+  },
 
-	showTaskFormat(task) {
-		if (!task.done) return '\x1b[31m%s\x1b[0m'
-		return '\x1b[32m%s\x1b[0m'
-	},
+  showTaskFormat(task) {
+    if (!task.done) return '\x1b[31m%s\x1b[0m'
+    return '\x1b[32m%s\x1b[0m'
+  },
 
-	shouldShow: all => t => !t.done || all,
+  shouldShow: all => t => !t.done || all,
 
-	unknownIdError(id) {
-		return new Error('Unknown task with id ' + id)
-	},
+  unknownIdError(id) {
+    return new Error('Unknown task with id ' + id)
+  },
 }
 
 const commands = {
-	add(tasks, {args: [task]}) {
-		if (!task) throw new Error('Params has no task')
+  add(tasks, { args: [task] }) {
+    if (!task) throw new Error('Params has no task')
 
-		const id = tasks.map(t => t.id).sort().reverse()[0] + 1 || 1
-		tasks.push({task, id, done: false})
-		return tasks
-	},
+    const id = tasks.map(t => t.id).sort().reverse()[0] + 1 || 1
+    tasks.push({ task, id, done: false })
+    return tasks
+  },
 
-	list(tasks, {options: {all}}) {
-		tasksUI.showTasks(tasks.filter(tasksUI.shouldShow(all)))
-	},
+  list(tasks, { options: { all } }) {
+    tasksUI.showTasks(tasks.filter(tasksUI.shouldShow(all)))
+  },
 
-	done(tasks, {args: [id]}) {
-		const task = tasks.find(t => t.id === +id)
-		if (!task) throw tasksUI.unknownIdError(id)
-		task.done = true
-		return tasks
-	},
+  done(tasks, { args: [id] }) {
+    const task = tasks.find(t => t.id === +id)
+    if (!task) throw tasksUI.unknownIdError(id)
+    task.done = true
+    return tasks
+  },
 
-	search(tasks, {args: [query], options: {all}}) {
-		const actual = tasks.filter(tasksUI.shouldShow(all))
-		const queryRegExp = new RegExp(query, 'ig')
-		tasksUI.showTasks(actual.filter(t => t.task.match(queryRegExp)))
-	},
+  search(tasks, { args: [query], options: { all } }) {
+    const actual = tasks.filter(tasksUI.shouldShow(all))
+    const queryRegExp = new RegExp(query, 'ig')
+    tasksUI.showTasks(actual.filter(t => t.task.match(queryRegExp)))
+  },
 
-	rm(tasks, {args: [id]}) {
-	},
+  rm(tasks, { args: [id] }) {
+  },
 }
 
 const main = async commandArgs => {
-	const commandName = commandArgs.args.shift()
-	const command = commands[commandName] || (() => console.log(`Unknown command: ${commandName}`))
-	const tasksData = await withFile(db, mode.read, handler => handler.readFile(encoding))
-	const tasks = tasksData ? JSON.parse(tasksData) : []
+  const commandName = commandArgs.args.shift()
+  const command = commands[commandName] || (() => console.log(`Unknown command: ${commandName}`))
+  const tasksData = await withFile(db, mode.read, handler => handler.readFile(encoding))
+  const tasks = tasksData ? JSON.parse(tasksData) : []
 
-	try {
-		const newTasks = await command(tasks, commandArgs)
-		if (newTasks) await withFile(db, mode.write, handler => handler.write(
-			JSON.stringify(newTasks, null, 2)
-		))
-	} catch (e) {
-		console.error(e.message)
-	}
+  try {
+    const newTasks = await command(tasks, commandArgs)
+    if (newTasks) await withFile(db, mode.write, handler => handler.write(
+      JSON.stringify(newTasks, null, 2)
+    ))
+  } catch (e) {
+    console.error(e.message)
+  }
 }
 
 main(parseArgs(process.argv.slice(2)))
